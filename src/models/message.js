76cdda6fe6012e945bb616cfd204001868f28b41
Starlight Romero <starlightromero@protonmail.com>
2021-02-12 09:41:40 -0800

    [upated] formatted

--- src/models/message.js
+++ src/models/message.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const MessageSchema = new Schema({
-    title: { type: String, required: true },
-    body: { type: String, required: true },
-    author : { type: Schema.Types.ObjectId, ref: "User", required: true },
+  title: { type: String, required: true },
+  body: { type: String, required: true },
+  author: { type: Schema.Types.ObjectId, ref: 'User', required: true }
 })
 
 const Message = mongoose.model('Message', MessageSchema)
