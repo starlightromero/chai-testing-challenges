c7d56b2ad5d46f2135a68a9524098098a0000a67
Starlight Romero <starlightromero@protonmail.com>
2021-02-16 19:47:03 -0800

    [update[ finish user tests

--- src/test/user.js
+++ src/test/user.js
@@ -13,9 +13,6 @@ const expect = chai.expect
 const should = chai.should()
 chai.use(chaiHttp)
 
-/**
- * root level hooks
- */
 after((done) => {
   // required because https://github.com/Automattic/mongoose/issues/1251#issuecomment-65793092
   mongoose.models = {}
@@ -27,98 +24,110 @@ after((done) => {
 const SAMPLE_OBJECT_ID = 'aaaaaaaaaaaa' // 12 byte string
 
 describe('User API endpoints', () => {
-    // Create a sample user for use in tests.
-    beforeEach((done) => {
-        const sampleUser = new User({
-            username: 'myuser',
-            password: 'mypassword',
-            _id: SAMPLE_OBJECT_ID
-        })
-        sampleUser.save()
-        .then(() => {
-            done()
-        })
+  // Create a sample user for use in tests.
+  beforeEach((done) => {
+    const sampleUser = new User({
+      username: 'myuser',
+      password: 'mypassword',
+      _id: SAMPLE_OBJECT_ID
     })
-
-    // Delete sample user.
-    afterEach((done) => {
-        User.deleteMany({ username: ['myuser', 'anotheruser'] })
-        .then(() => {
-            done()
-        })
+    sampleUser.save().then(() => {
+      done()
     })
+  })
 
-    it('should load all users', (done) => {
-        chai.request(app)
-        .get('/users')
-        .end((err, res) => {
-            if (err) { done(err) }
-            expect(res).to.have.status(200)
-            expect(res.body.users).to.be.an("array")
-            done()
-        })
+  // Delete sample user.
+  afterEach(done => {
+    User.deleteMany(
+      { username: ['myuser', 'anotheruser'] }
+    ).then(() => {
+      done()
     })
+  })
 
-    it('should get one user', (done) => {
-        chai.request(app)
-        .get(`/users/${SAMPLE_OBJECT_ID}`)
-        .end((err, res) => {
-            if (err) { done(err) }
-            expect(res).to.have.status(200)
-            expect(res.body).to.be.an('object')
-            expect(res.body.username).to.equal('myuser')
-            expect(res.body.password).to.equal(undefined)
-            done()
-        })
+  it('should load all users', (done) => {
+    chai.request(
+      app
+    ).get(
+      '/users'
+    ).end((err, res) => {
+      if (err) { done(err) }
+      expect(res).to.have.status(200)
+      expect(res.body.users).to.be.an('array')
+      done()
     })
+  })
 
-    it('should post a new user', (done) => {
-        chai.request(app)
-        .post('/users')
-        .send({username: 'anotheruser', password: 'mypassword'})
-        .end((err, res) => {
-            if (err) { done(err) }
-            expect(res.body.user).to.be.an('object')
-            expect(res.body.user).to.have.property('username', 'anotheruser')
-
-            // check that user is actually inserted into database
-            User.findOne({username: 'anotheruser'}).then(user => {
-                expect(user).to.be.an('object')
-                done()
-            })
-        })
+  it('should get one user', (done) => {
+    chai.request(
+      app
+    ).get(
+      `/users/${SAMPLE_OBJECT_ID}`
+    ).end((err, res) => {
+      if (err) { done(err) }
+      expect(res).to.have.status(200)
+      expect(res.body).to.be.an('object')
+      expect(res.body.username).to.equal('myuser')
+      expect(res.body.password).to.equal(undefined)
+      done()
     })
+  })
 
-    it('should update a user', (done) => {
-        chai.request(app)
-        .put(`/users/${SAMPLE_OBJECT_ID}`)
-        .send({username: 'anotheruser'})
-        .end((err, res) => {
-            if (err) { done(err) }
-            expect(res.body.user).to.be.an('object')
-            expect(res.body.user).to.have.property('username', 'anotheruser')
+  it('should post a new user', done => {
+    chai.request(
+      app
+    ).post(
+      '/users'
+    ).send(
+      { username: 'anotheruser', password: 'mypassword' }
+    ).end((err, res) => {
+      if (err) { done(err) }
+      expect(res.body.user).to.be.an('object')
+      expect(res.body.user).to.have.property('username', 'anotheruser')
 
-            // check that user is actually inserted into database
-            User.findOne({username: 'anotheruser'}).then(user => {
-                expect(user).to.be.an('object')
-                done()
-            })
-        })
+      // check that user is actually inserted into database
+      User.findOne({ username: 'anotheruser' }).then(user => {
+        expect(user).to.be.an('object')
+        done()
+      })
     })
+  })
 
-    it('should delete a user', (done) => {
-        chai.request(app)
-        .delete(`/users/${SAMPLE_OBJECT_ID}`)
-        .end((err, res) => {
-            if (err) { done(err) }
-            expect(res.body.message).to.equal('Successfully deleted.')
-            expect(res.body._id).to.equal(SAMPLE_OBJECT_ID)
+  it('should update a user', (done) => {
+    chai.request(
+      app
+    ).put(
+      `/users/${SAMPLE_OBJECT_ID}`
+    ).send(
+      { username: 'anotheruser' }
+    ).end((err, res) => {
+      if (err) { done(err) }
+      expect(res.body.user).to.be.an('object')
+      expect(res.body.user).to.have.property('username', 'anotheruser')
 
-            // check that user is actually deleted from database
-            User.findOne({username: 'myuser'}).then(user => {
-                expect(user).to.equal(null)
-                done()
-            })
-        })
+      // check that user is actually inserted into database
+      User.findOne({ username: 'anotheruser' }).then(user => {
+        expect(user).to.be.an('object')
+        done()
+      })
     })
+  })
+
+  it('should delete a user', (done) => {
+    chai.request(
+      app
+    ).delete(
+      `/users/${SAMPLE_OBJECT_ID}`
+    ).end((err, res) => {
+      if (err) { done(err) }
+      expect(res.body.message).to.equal('Successfully deleted.')
+      expect(res.body._id).to.equal(SAMPLE_OBJECT_ID)
+
+      // check that user is actually deleted from database
+      User.findOne({ username: 'myuser' }).then(user => {
+        expect(user).to.equal(null)
+        done()
+      })
+    })
+  })
 })
